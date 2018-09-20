44fa427b4f9a435e89ee117553711970c6a0f29c
Ankit Tiwari <ankitt255@gmail.com>
2018-09-21 00:19:04 +0530

    Add all files

---
+++ index.js
@@ -0,0 +1,22 @@
+const electron = require("electron");
+const ffmpeg = require("fluent-ffmpeg");
+
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
+
+// wait until app is ready
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/client/index.html`);
+});
+
+ipcMain.on("video:submit", (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    if (err) {
+      return console.error("Some error occurred with ffmpeg");
+    }
+    const duration = metadata.format.duration;
+    mainWindow.webContents.send("video:duration", duration);
+  });
+});
